1df9440f4e30520d4c202d30c4c02dafd84a5a38
mfxod <46651745+mfxod@users.noreply.github.com>
2019-07-26 19:08:19 -0400

    gets stockQuantity based on Inquirer input

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,7 +18,7 @@ function queryProducts() {
             // console.log(res);
             console.log("Hawkins Department Store Featured Items: \n");
             for (let i = 0; i < res.length; i++) {
-                console.log(res[i].itemID + "\t" + res[i].productName + "  \($" + res[i].price + "\)");
+                console.log(res[i].itemID + "\t" + res[i].productName + " \($" + res[i].price + "\)");
             }
         }
     });
@@ -28,26 +28,36 @@ connection.connect(function (err) {
     if (err) {
         throw err
     };
-    console.log("Connected as id " + connection.threadId + "\n");
-    queryProducts();
-    connection.end();
+    // WHY DOES THIS CONSOLE LOG BREAK THE INQUIRER LIST?
+    // console.log("Connected as id " + connection.threadId + "\n");
+
+    // queryProducts();
+    // connection.end();
 });
 
 //   ===== INQUIRER PROMPTS =====
-// inquirer
-//     .prompt([
-//         {
-//             type: "list",
-//             message: "Enter the itemID of the product you would like to buy.",
-//             choices: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-//             name: "product"
-//         },
-//         {
-//             type: "number",
-//             message: "Enter the number of units you would like to buy.",
-//             name: "quantity"
-//         },
-// ])
-//     .then(function (inquirerRes) {
-//         console.log(inquirerRes)
-//     });
+// console.log(inquirer.prompt);
+inquirer
+    .prompt([
+        {
+            type: "list",
+            message: "Choose the itemID of the product you would like to buy.",
+            choices: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            name: "itemID"
+        },
+        {
+            type: "number",
+            message: "Enter the number of units you would like to buy.",
+            name: "quantity"
+        },
+])
+    .then(function (iRes) {
+        connection.query("SELECT stockQuantity FROM products WHERE itemID = " + iRes.itemID, function(err, res) {
+            if (err) {
+                throw err;
+            } else {
+                console.log(res);
+                connection.end();
+            }
+        });
+    });
