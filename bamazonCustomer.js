4149421c08958c6cbcb4cb9d22157faff72ad183
mfxod <46651745+mfxod@users.noreply.github.com>
2019-07-26 17:04:01 -0400

    connection estd. & .query() working

---
+++ bamazonCustomer.js
@@ -0,0 +1,57 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+// ===== ESTABLISH CONNECTION AND SHOW PRODUCTS =====
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bamazondb"
+});
+
+function queryProducts() {
+    connection.query("SELECT itemID, productName, CONCAT(price) FROM products", function(err, res) {
+        if (err) {
+            throw err;
+        } else {
+            console.log(res);
+            // console.log("Hawkins Department Store Featured Items: \n");
+            // for (let i = 0; i < res.length; i++) {
+            //     console.log(
+            //         res[i].itemID + 
+            //         "   " + res[i].productName + 
+            //         "   " + res[i].price
+            //     );
+            // }
+        }
+    });
+};
+
+connection.connect(function (err) {
+    if (err) {
+        throw err
+    };
+    console.log("Connected as id " + connection.threadId + "\n");
+    queryProducts();
+    connection.end();
+});
+
+//   ===== INQUIRER PROMPTS =====
+// inquirer
+//     .prompt([
+//         {
+//             type: "list",
+//             message: "Enter the itemID of the product you would like to buy.",
+//             choices: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+//             name: "product"
+//         },
+//         {
+//             type: "number",
+//             message: "Enter the number of units you would like to buy.",
+//             name: "quantity"
+//         },
+// ])
+//     .then(function (inquirerRes) {
+//         console.log(inquirerRes)
+//     });
